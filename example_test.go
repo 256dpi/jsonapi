a97a79ff6969b65643c0a9376283e93e45589a5d
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-09-01 10:12:04 +0200

    added basic example

---
+++ example_test.go
@@ -0,0 +1,38 @@
+package jsonapi
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/parnurzeal/gorequest"
+)
+
+func Example() {
+	http.HandleFunc("/api/posts/1", func(w http.ResponseWriter, r *http.Request) {
+		req, err := ParseRequest(r, "/api/")
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(req.Resource)
+		fmt.Println(req.ResourceID)
+	})
+
+	go func() {
+		http.ListenAndServe("0.0.0.0:4040", nil)
+	}()
+
+	_, str, err := gorequest.New().
+		Get("http://0.0.0.0:4040/api/posts/1").
+		Set("Content-Type", ContentType).
+		End()
+	if err != nil {
+		panic(err[0])
+	}
+
+	fmt.Println(str)
+
+	// Output:
+	// posts
+	// 1
+}
