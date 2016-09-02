7f3e75d5258f53d3bfb32132404f00d8d2669630
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-09-02 17:27:05 +0200

    implement example without gin

--- example/main.go
+++ example/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/gonfire/jsonapi"
 )
+
+var counter = 1
+var store = make(map[string]*postModel)
 
 type postModel struct {
 	ID    string
@@ -14,123 +18,120 @@ type postModel struct {
 }
 
 func main() {
-	router := gin.Default()
+	http.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
 
-	counter := 1
-	store := make(map[string]*postModel)
+		defer func() {
+			fmt.Printf("%s %s %s\n", pad(r.Method, 7), pad(r.URL.Path, 15), time.Since(start).String())
+		}()
 
-	router.GET("/api/posts", func(ctx *gin.Context) {
-		_, err := jsonapi.ParseRequest(ctx.Request, "/api/")
+		req, err := jsonapi.ParseRequest(r, "/api/")
 		if err != nil {
-			jsonapi.WriteError(ctx.Writer, err)
+			jsonapi.WriteError(w, err)
 			return
 		}
 
-		list := make([]*jsonapi.Resource, 0)
-		for _, post := range store {
-			list = append(list, &jsonapi.Resource{
-				Type: "posts",
-				ID:   post.ID,
-				Attributes: jsonapi.Map{
-					"title": post.Title,
-				},
-			})
+		var doc *jsonapi.Document
+		if req.DocumentExpected() {
+			doc, err = jsonapi.ParseBody(r.Body)
+			if err != nil {
+				jsonapi.WriteError(w, err)
+				return
+			}
 		}
 
-		jsonapi.WriteResponse(ctx.Writer, http.StatusOK, &jsonapi.Document{
-			Data: &jsonapi.HybridResource{
-				Many: list,
-			},
-		})
+		if req.Action == jsonapi.Fetch {
+			if req.Target == jsonapi.ResourceCollection {
+				fetchPostList(req, w)
+				return
+			} else if req.Target == jsonapi.SingleResource {
+				fetchSinglePost(req, w)
+				return
+			}
+		} else if req.Action == jsonapi.Create && req.Target == jsonapi.ResourceCollection {
+			createPost(req, doc, w)
+			return
+		} else if req.Action == jsonapi.Update && req.Target == jsonapi.SingleResource {
+			updatePost(req, doc, w)
+			return
+		} else if req.Action == jsonapi.Delete && req.Target == jsonapi.SingleResource {
+			deletePost(req, w)
+			return
+		}
+
+		jsonapi.WriteErrorFromStatus(w, http.StatusBadRequest)
 	})
 
-	router.POST("/api/posts", func(ctx *gin.Context) {
-		_, err := jsonapi.ParseRequest(ctx.Request, "/api/")
-		if err != nil {
-			jsonapi.WriteError(ctx.Writer, err)
-			return
-		}
-
-		doc, err := jsonapi.ParseBody(ctx.Request.Body)
-		if err != nil {
-			jsonapi.WriteError(ctx.Writer, err)
-			return
-		}
-
-		post := &postModel{
-			ID:    strconv.Itoa(counter),
-			Title: doc.Data.One.Attributes["title"].(string),
-		}
-
-		counter++
-		store[post.ID] = post
-
-		writePost(ctx, post)
-	})
-
-	router.GET("/api/posts/:id", func(ctx *gin.Context) {
-		req, err := jsonapi.ParseRequest(ctx.Request, "/api/")
-		if err != nil {
-			jsonapi.WriteError(ctx.Writer, err)
-			return
-		}
-
-		post, ok := store[req.ResourceID]
-		if !ok {
-			jsonapi.WriteErrorFromStatus(ctx.Writer, http.StatusNotFound)
-			return
-		}
-
-		writePost(ctx, post)
-	})
-
-	router.PATCH("/api/posts/:id", func(ctx *gin.Context) {
-		req, err := jsonapi.ParseRequest(ctx.Request, "/api/")
-		if err != nil {
-			jsonapi.WriteError(ctx.Writer, err)
-			return
-		}
-
-		doc, err := jsonapi.ParseBody(ctx.Request.Body)
-		if err != nil {
-			jsonapi.WriteError(ctx.Writer, err)
-			return
-		}
-
-		post, ok := store[req.ResourceID]
-		if !ok {
-			jsonapi.WriteErrorFromStatus(ctx.Writer, http.StatusNotFound)
-			return
-		}
-
-		post.Title = doc.Data.One.Attributes["title"].(string)
-
-		writePost(ctx, post)
-	})
-
-	router.DELETE("/api/posts/:id", func(ctx *gin.Context) {
-		req, err := jsonapi.ParseRequest(ctx.Request, "/api/")
-		if err != nil {
-			jsonapi.WriteError(ctx.Writer, err)
-			return
-		}
-
-		_, ok := store[req.ResourceID]
-		if !ok {
-			jsonapi.WriteErrorFromStatus(ctx.Writer, http.StatusNotFound)
-			return
-		}
-
-		delete(store, req.ResourceID)
-
-		ctx.Status(http.StatusOK)
-	})
-
-	router.Run("0.0.0.0:4000")
+	http.ListenAndServe("0.0.0.0:4000", nil)
 }
 
-func writePost(ctx *gin.Context, post *postModel) {
-	jsonapi.WriteResponse(ctx.Writer, http.StatusOK, &jsonapi.Document{
+func fetchPostList(_ *jsonapi.Request, w http.ResponseWriter) {
+	list := make([]*jsonapi.Resource, 0)
+	for _, post := range store {
+		list = append(list, &jsonapi.Resource{
+			Type: "posts",
+			ID:   post.ID,
+			Attributes: jsonapi.Map{
+				"title": post.Title,
+			},
+		})
+	}
+
+	jsonapi.WriteResponse(w, http.StatusOK, &jsonapi.Document{
+		Data: &jsonapi.HybridResource{
+			Many: list,
+		},
+	})
+}
+
+func fetchSinglePost(req *jsonapi.Request, w http.ResponseWriter) {
+	post, ok := store[req.ResourceID]
+	if !ok {
+		jsonapi.WriteErrorFromStatus(w, http.StatusNotFound)
+		return
+	}
+
+	writePost(w, post)
+}
+
+func createPost(_ *jsonapi.Request, doc *jsonapi.Document, w http.ResponseWriter) {
+	post := &postModel{
+		ID:    strconv.Itoa(counter),
+		Title: doc.Data.One.Attributes["title"].(string),
+	}
+
+	counter++
+	store[post.ID] = post
+
+	writePost(w, post)
+}
+
+func updatePost(req *jsonapi.Request, doc *jsonapi.Document, w http.ResponseWriter) {
+	post, ok := store[req.ResourceID]
+	if !ok {
+		jsonapi.WriteErrorFromStatus(w, http.StatusNotFound)
+		return
+	}
+
+	post.Title = doc.Data.One.Attributes["title"].(string)
+
+	writePost(w, post)
+}
+
+func deletePost(req *jsonapi.Request, w http.ResponseWriter) {
+	_, ok := store[req.ResourceID]
+	if !ok {
+		jsonapi.WriteErrorFromStatus(w, http.StatusNotFound)
+		return
+	}
+
+	delete(store, req.ResourceID)
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func writePost(w http.ResponseWriter, post *postModel) {
+	jsonapi.WriteResponse(w, http.StatusOK, &jsonapi.Document{
 		Data: &jsonapi.HybridResource{
 			One: &jsonapi.Resource{
 				Type: "posts",
@@ -141,4 +142,16 @@ func writePost(ctx *gin.Context, post *postModel) {
 			},
 		},
 	})
+}
+
+func pad(str string, n int) string {
+	for {
+		if len(str) < n {
+			str += " "
+		}
+
+		if len(str) >= n {
+			return str
+		}
+	}
 }
