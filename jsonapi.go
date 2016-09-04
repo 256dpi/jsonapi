ca28df58c88619910830dc087ec972f0d9c1bc00
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-09-04 13:55:32 +0200

    combine files

--- jsonapi.go
+++ jsonapi.go
@@ -2,9 +2,32 @@
 // compatible APIs.
 package jsonapi
 
-var objectSuffix = []byte("{")
-var arraySuffix = []byte("[")
+import "net/http"
 
 // MediaType is the official JSON API media type that should be used by
 // all requests and responses.
 const MediaType = "application/vnd.api+json"
+
+var objectSuffix = []byte("{")
+var arraySuffix = []byte("[")
+
+// returns a bad request error
+func badRequest(detail string) *Error {
+	return &Error{
+		Status: http.StatusBadRequest,
+		Title:  http.StatusText(http.StatusBadRequest),
+		Detail: detail,
+	}
+}
+
+// returns a bad request error with a parameter source
+func badRequestParam(detail, param string) *Error {
+	return &Error{
+		Status: http.StatusBadRequest,
+		Title:  http.StatusText(http.StatusBadRequest),
+		Detail: detail,
+		Source: &ErrorSource{
+			Parameter: param,
+		},
+	}
+}
